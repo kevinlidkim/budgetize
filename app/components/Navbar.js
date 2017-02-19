99e0e9e81c0c05792051ff4965949e2aefe9a988
kevin <kevin.li.3@stonybrook.edu>
2017-02-19 01:53:49 -0500

    changed more styling

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import {Link} from 'react-router';
-import NavbarStore from '../stores/NavbarStore';
-import NavbarActions from '../actions/NavbarActions';
 import ProfileStore from '../stores/ProfileStore'
 
 class Navbar extends React.Component {
@@ -14,31 +12,15 @@ class Navbar extends React.Component {
   }
 
   componentDidMount() {
-    NavbarStore.listen(this.onChange);
     ProfileStore.listen(this.onChange);
   }
 
   componentWillUnmount() {
-    NavbarStore.unlisten(this.onChange);
     ProfileStore.unlisten(this.onChange);
   }
 
   onChange(state) {
     this.setState({budget: ProfileStore.state.budget});
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-
-    let searchQuery = this.state.searchQuery.trim();
-
-    if (searchQuery) {
-      NavbarActions.findCharacter({
-        searchQuery: searchQuery,
-        searchForm: this.refs.searchForm,
-        history: this.props.history
-      });
-    }
   }
 
   render() {
@@ -51,14 +33,6 @@ class Navbar extends React.Component {
           </Link>
         </div>
         <div id='navbar' className='navbar-collapse collapse'>
-          <form ref='searchForm' className='navbar-form navbar-left animated' onSubmit={this.handleSubmit.bind(this)}>
-            <div className='input-group'>
-              <input type='text' className='form-control' placeholder='Search' value={this.state.searchQuery} onChange={NavbarActions.updateSearchQuery} />
-              <span className='input-group-btn'>
-                <button className='btn btn-default' onClick={this.handleSubmit.bind(this)}><span className='glyphicon glyphicon-search'></span></button>
-              </span>
-            </div>
-          </form>
           <ul className='nav navbar-nav'>
             <li><Link to='/login'>Login</Link></li>
             <li><Link to='/profile'>Profile</Link></li>
--- app/components/SaleItem.js
+++ app/components/SaleItem.js
@@ -23,16 +23,16 @@ class SaleItem extends React.Component {
     if (budget > price) {
       ProfileActions.buyItem(this.props.item_name, this.props.price, this.props.payment_token, this.props.access_token, LoginStore.state.user);
     } else {
-      alert("get some money kid");
+      alert("Insufficient funds");
       // console.log("YOU DONT HAVE ENOUGH MONEY");
     }
   }
 
   render() {
     return (
-      <div>
+      <div className="alert alert-info">
         <h4>{this.props.item_name}</h4>
-        <img src={this.props.item_url} className="rounded"></img>
+        <img src={this.props.item_url} className="rounded" style={{maxWidth: 400}}></img>
         <h5>{this.props.price}</h5>
         <button type="submit" className="btn btn-default" onClick={this.buyItem.bind(this)}>Purchase</button>
       </div>
